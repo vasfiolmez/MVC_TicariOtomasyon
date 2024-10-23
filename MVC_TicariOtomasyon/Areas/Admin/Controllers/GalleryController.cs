75e70f6d22c9214d272cfd9a0b163ac5f9e8a835
Vasfi Ölmez <58458944+vasfiolmez@users.noreply.github.com>
2024-10-23 21:52:01 +0300

    gallery and istatistics

---
+++ MVC_TicariOtomasyon/Areas/Admin/Controllers/GalleryController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_TicariOtomasyon.Areas.Admin.Controllers
+{
+    public class GalleryController : Controller
+    {
+        // GET: Admin/Gallery
+        public ActionResult Index()
+        {
+            ViewBag.baslik = "Galeri Sayfası";
+            return View();
+        }
+    }
+}
---
+++ MVC_TicariOtomasyon/Areas/Admin/Controllers/IstatisticsController.cs
@@ -0,0 +1,36 @@
+﻿using MVC_TicariOtomasyon.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_TicariOtomasyon.Areas.Admin.Controllers
+{
+    public class IstatisticsController : Controller
+    {
+        TicariOtomasyonContext context = new TicariOtomasyonContext();
+
+        public ActionResult IstatisticsList()
+        {
+            ViewBag.baslik = "İstatistikler";
+            ViewBag.musteriSayisi = context.Customers.Count().ToString();
+            ViewBag.urunSayisi = context.Products.Count().ToString();
+            ViewBag.personelSayisi = context.Employees.Count().ToString();
+            ViewBag.kategoriSayisi = context.Categories.Count().ToString();
+            ViewBag.stokSayisi = context.Products.Sum(x => x.Stock).ToString();
+            ViewBag.markaSayisi = context.Products.Select(x => x.Brand).Count().ToString();
+            ViewBag.kritikSeviye = context.Products.Where(x => x.Stock < 50).Select(x=>x.ProductId).Count().ToString();
+            ViewBag.fiyatiEnYuksekOlanUrun = context.Products.OrderByDescending(x => x.SalePrice).Select(x => x.ProductName).FirstOrDefault();
+            ViewBag.fiyatiEnDusukOlanUrun = context.Products.OrderBy(x => x.SalePrice).Select(x => x.ProductName).FirstOrDefault();
+            ViewBag.enFazlaUrunluMarka = context.Products.GroupBy(x => x.Brand).OrderByDescending(x => x.Count()).Select(y => y.Key).FirstOrDefault();
+            ViewBag.buzdolabıSayısı = context.Products.Where(x => x.ProductName == "Buzdolabı").Select(x => x.Stock).FirstOrDefault();
+            ViewBag.bilgisayarSayısı = context.Products.Where(x => x.Category.Name == "Bilgisayar").Select(x => x.Stock).FirstOrDefault();
+            ViewBag.enCokSatanUrun = context.SalesMovements.OrderByDescending(x => x.Piece).Select(x => x.Product.ProductName).FirstOrDefault();
+            ViewBag.kasadakiTutar = context.SalesMovements.Sum(x => x.Amount).ToString();
+            ViewBag.bugunkuSatis = context.SalesMovements.Where(x => x.Date == DateTime.Today).Select(x => x.Piece).Count().ToString();
+            ViewBag.bugunkuKazanc = context.SalesMovements.Where(x => x.Date == DateTime.Today).Sum(x => x.Amount).ToString();
+            return View();
+        }
+    }
+}
