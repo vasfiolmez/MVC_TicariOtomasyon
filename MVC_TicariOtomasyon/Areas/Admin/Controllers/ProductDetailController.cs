619f872b82396705fcace7b08f272b9fbe797864
Vasfi Ölmez <58458944+vasfiolmez@users.noreply.github.com>
2024-10-23 23:59:26 +0300

    .

---
+++ MVC_TicariOtomasyon/Areas/Admin/Controllers/ProductDetailController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_TicariOtomasyon.Areas.Admin.Controllers
+{
+    public class ProductDetailController : Controller
+    {
+        // GET: Admin/ProductDetail
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
