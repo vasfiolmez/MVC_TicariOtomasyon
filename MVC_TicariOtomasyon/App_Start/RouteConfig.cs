0d62fe6d3f77115b23aa4f8f8c8c3493f1ad2016
Vasfi Ölmez <58458944+vasfiolmez@users.noreply.github.com>
2025-01-02 22:20:28 +0300

    .

--- MVC_TicariOtomasyon/App_Start/RouteConfig.cs
+++ MVC_TicariOtomasyon/App_Start/RouteConfig.cs
@@ -18,6 +18,11 @@ namespace MVC_TicariOtomasyon
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            routes.MapRoute(
+               name: "CargoDetails",
+               url: "Admin/Cargo/CargoDetail/{trackingcode}",
+               defaults: new { controller = "Cargo", action = "CargoDetail",Areas="Admin", trackingcode = UrlParameter.Optional }
+           );
         }
     }
 }
--- MVC_TicariOtomasyon/Areas/Admin/Controllers/GrafikController.cs
+++ MVC_TicariOtomasyon/Areas/Admin/Controllers/GrafikController.cs
@@ -6,9 +6,9 @@ using System.Web.Mvc;
 
 namespace MVC_TicariOtomasyon.Areas.Admin.Controllers
 {
-    public class AlertsController : Controller
+    public class GrafikController : Controller
     {
-        // GET: Admin/Alert
+        // GET: Admin/Grafik
         public ActionResult Index()
         {
             return View();
