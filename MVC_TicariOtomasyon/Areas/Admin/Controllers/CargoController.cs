078a3f2cb4ea7d38cdda3774d15db6e68f011a73
Vasfi Ölmez <58458944+vasfiolmez@users.noreply.github.com>
2025-01-02 22:20:20 +0300

    kargo ve qr

--- MVC_TicariOtomasyon/Areas/Admin/Controllers/CargoController.cs
+++ MVC_TicariOtomasyon/Areas/Admin/Controllers/CargoController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MVC_TicariOtomasyon.Context;
+using MVC_TicariOtomasyon.Models.Entities;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +11,63 @@ namespace MVC_TicariOtomasyon.Areas.Admin.Controllers
 {
     public class CargoController : Controller
     {
-        // GET: Admin/CargoDetail
-        public ActionResult Index()
+        TicariOtomasyonContext context=new TicariOtomasyonContext();
+        public ActionResult Index(string kargoTakipNo)
         {
+            ViewBag.baslik = "Kargo Listesi";
+            var values= from x in context.CargoDetails select x;
+            if (!String.IsNullOrEmpty(kargoTakipNo))
+            {
+                values = values.Where(x => x.TrackingCode.Contains(kargoTakipNo));
+            }
+
+            return View(values.ToList());
+        }
+        [HttpGet]
+        public ActionResult NewCargoAdd()
+        {
+            ViewBag.baslik = "Kargo Bilgisi Ekle";
+            var trackingCode = context.CargoDetails.Select(c => c.TrackingCode).ToString();
+            trackingCode = GenerateTrackingCode();
+            ViewBag.trackingCode = trackingCode;    
             return View();
+        }
+        [HttpPost]
+        public ActionResult NewCargoAdd(CargoDetail cargo)
+        {
+            
+            context.CargoDetails.Add(cargo);
+            context.SaveChanges();
+            return RedirectToAction("Index", "Cargo", "Admin");
+        }
+
+        public ActionResult CargoDetail(string trackingcode)
+        {
+            ViewBag.baslik = "Kargo Detay";
+
+            var code =context.CargoTrackings.Where(x=>x.TrackingCode==trackingcode).ToList();
+            ViewBag.code = trackingcode;
+            return View(code);
+        }
+
+        private string GenerateTrackingCode()
+        {
+            // Veritabanında benzersizliği kontrol edin (önerilir, özellikle kısa kodlar için)
+            while (true)
+            {
+                string trackingCode = GenerateGuidCode(10);
+                                                     
+                if (!context.CargoDetails.Any(c => c.TrackingCode == trackingCode))
+                {
+                    return trackingCode;
+                }
+            }
+        }
+
+        private string GenerateGuidCode(int length)
+        {
+            string guid = Guid.NewGuid().ToString("N"); // "N" formatı, tiresiz ve büyük harfli bir GUID üretir
+            return guid.Substring(0, Math.Min(guid.Length, length)); // İstenilen uzunlukta kısalt
         }
     }
 }
---
+++ MVC_TicariOtomasyon/Areas/Admin/Controllers/QrCodeController.cs
@@ -0,0 +1,56 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using static QRCoder.QRCodeGenerator;
+using System.Text.RegularExpressions;
+
+namespace MVC_TicariOtomasyon.Areas.Admin.Controllers
+{
+    public class QrCodeController : Controller
+    {
+        public ActionResult GenerateQRCode()
+        {
+            ViewBag.baslik = "QR Kod";
+            return View();
+        }
+            [HttpPost]
+        public ActionResult GenerateQRCode(string qrCodeContent)
+        {
+            ViewBag.baslik = "QR Kod";
+            if (string.IsNullOrEmpty(qrCodeContent))
+            {
+                ViewBag.QrCodeImage = "";
+                // veya  ViewBag.QrCodeImage = "";
+                return View();
+            }
+
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(qrCodeContent, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                qrCodeImage.Save(ms, ImageFormat.Png);
+                byte[] imageBytes = ms.ToArray();
+                string base64Image = "data:image/png;base64," + Convert.ToBase64String(imageBytes);
+
+                ViewBag.QrCodeImage = base64Image; 
+            }
+
+            string fileName = $"{qrCodeContent}.png";   
+
+            fileName = Regex.Replace(fileName, "[^a-zA-Z0-9_\\.-]", "_");
+            ViewBag.QrCodeFileName = fileName;
+
+            return View(); // Aynı View'ı döndür
+        }
+       
+    }
+}
