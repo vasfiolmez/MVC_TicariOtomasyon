fb7ed73791109e671f28494dcd68e2c415e6b8eb
Vasfi Ölmez <58458944+vasfiolmez@users.noreply.github.com>
2025-01-03 23:42:34 +0300

    Message

---
+++ MVC_TicariOtomasyon/Areas/CustomerPanel/Controllers/MessageController.cs
@@ -0,0 +1,47 @@
+﻿using MVC_TicariOtomasyon.Context;
+using MVC_TicariOtomasyon.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_TicariOtomasyon.Areas.CustomerPanel.Controllers
+{
+    public class MessageController : Controller
+    {
+        TicariOtomasyonContext _context=new TicariOtomasyonContext();
+        public ActionResult MessageList()
+        {
+
+            ViewBag.baslik = "Mesajlar";
+            var mail = (string)Session["CustomerMail"];
+            var values = _context.Messages.Where(x=>x.Receiver==mail).ToList();
+            var inboxCount=_context.Messages.Count(x=>x.Receiver==mail);
+            var sendboxCount = _context.Messages.Count(x => x.Sender == mail);
+            ViewBag.inboxCount = inboxCount;
+            ViewBag.sendboxCount = sendboxCount;
+            return View(values);
+        }
+        public ActionResult SendMessageBox()
+        {
+            var mail = (string)Session["CustomerMail"];
+            var values = _context.Messages.Where(x => x.Sender == mail).ToList();
+            var inboxCount = _context.Messages.Count(x => x.Receiver == mail);
+            var sendboxCount = _context.Messages.Count(x => x.Sender == mail);
+            ViewBag.inboxCount = inboxCount;
+            ViewBag.sendboxCount = sendboxCount;
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult NewMessage() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewMessage(Message message) 
+        { 
+        return View("Error");
+        }
+    }
+}
---
+++ MVC_TicariOtomasyon/Models/Entities/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_TicariOtomasyon.Models.Entities
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public DateTime SendDate { get; set; }
+        public bool IsRead { get; set; }
+
+    }
+}
